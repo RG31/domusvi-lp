4aba9eac7beef121d7c477662053476b6271d43d
eniger31 <regina.garcia31@gmail.com>
2021-05-06 10:35:02 +0200

    START

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import { Features } from './components/features'
+import { Services } from './components/services'
+import { Gallery } from './components/gallery'
+import { Contact } from './components/contact'
+import JsonData from './data/data.json'
+import SmoothScroll from 'smooth-scroll'
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+})
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({})
+  useEffect(() => {
+    setLandingPageData(JsonData)
+  }, [])
+
+  return (
+    <div>
+      <Contact data={landingPageData.Contact} />
+      <Features data={landingPageData.Features} />
+      <Services data={landingPageData.Services} />
+      <Gallery />
+    </div>
+  )
+}
+
+export default App
